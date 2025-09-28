34725f5cd29d79f1f301f0eadb64460eee992028
Mobashra Abeer <abeermobashra04@gmail.com>
2025-09-29 01:35:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Open Meteo App", layout="wide")
 
-st.title("🌍 Open Meteo Data Explorer")
+st.title("🌍 HIIIIIII Open Meteo Data Explorer")
 st.write("Welcome to the Open Meteo data app. Use the sidebar to navigate between pages.")
 
 st.sidebar.title("Navigation")
