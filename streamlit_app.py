000cfd84ba3bcd716a263422b79c7d4762fe1c6a
Mobashra Abeer <abeermobashra04@gmail.com>
2025-10-02 01:43:49 +0200

    design change

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,16 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title="Open Meteo App", layout="wide")
+st.set_page_config(page_title="IND320 PROJECT", layout="wide")
 
-st.title("🌍 Open Meteo Data Explorer")
-st.write("Welcome to the Open Meteo data app. Use the sidebar to navigate between pages.")
+st.title("WELCOME TO THE IND320 PROJECT APP")
+st.write("Here, you can explore the weather dataset through various interactive pages.")
+st.markdown(
+    """
+    **Dataset**  
+    - `open-meteo-subset.csv`: A subset of weather data from Open-Meteo, including variables like temperature, humidity, wind speed, and more.
+    """
+)
 
-st.sidebar.title("Navigation")
-st.sidebar.markdown("Select a page from the sidebar menu to explore the data.")
+st.sidebar.title("Pages")
+#st.sidebar.markdown("Select a page from the sidebar menu to explore the data.")
