50addabd7f00ec53ab6272405b59fcae82c662b3
Mobashra Abeer <abeermobashra04@gmail.com>
2025-10-01 12:49:53 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 
 # Optional -- adds the title and icon to the current page
-add_page_title()
+#add_page_title()
 
 # Specify what pages should be shown in the sidebar, and what their titles 
 # and icons should be
 show_pages(
     [
-        Page("streamlit_app.py", "Home", "🏠"),
+        #Page("streamlit_app.py", "Home", "🏠"),
         
         Page("pages/second page.py", "Page 2", "Data Table"),
     ]
