0361287657a8c37a5d73ee0412b0cc2175aa6016
Mobashra Abeer <abeermobashra04@gmail.com>
2025-10-02 16:50:34 +0200

    .

--- pages/4_Meme_[page 4].py
+++ pages/4_Meme_[page 4].py
@@ -6,20 +6,6 @@ st.title("🎉 Meme Page")
 # Create meme image
 width, height = 600, 400
 img = Image.new("RGB", (width, height), color="white")
-draw = ImageDraw.Draw(img)
 
-# Big bold text
-
-text_bottom = "ALL DEADLINES ARE ON MY BIRTHDAY 🎂"
-
-# Load a default font
-try:
-    font = ImageFont.truetype("arial.ttf", 28)
-except:
-    font = ImageFont.load_default()
-
-# Positioning
-#draw.text((50, 100), text_top, fill="black", font=font)
-draw.text((50, 200), text_bottom, fill="red", font=font)
 
 st.image(img, caption="When deadlines and birthdays collide 💀")
