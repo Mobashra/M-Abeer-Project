d0a5d2020a3e9e2ccefaeb54164cad98a2980a4e
Mobashra Abeer <abeermobashra04@gmail.com>
2025-10-02 16:52:02 +0200

    .

--- pages/4_Meme_[page 4].py
+++ pages/4_Meme_[page 4].py
@@ -1,11 +1,3 @@
 import streamlit as st
-from PIL import Image, ImageDraw, ImageFont
 
-st.title("🎉 Meme Page")
-
-# Create meme image
-width, height = 600, 400
-img = Image.new("RGB", (width, height), color="white")
-
-
-st.image(img, caption="When deadlines and birthdays collide 💀")
+st.header("When deadlines and birthdays collide 💀")
