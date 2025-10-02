40c9e463828fc3258d4630c81a8438ef9ebde26c
Mobashra Abeer <abeermobashra04@gmail.com>
2025-10-02 16:45:32 +0200

    .

--- pages/3_Data Plot_[page 3].py
+++ pages/3_Data Plot_[page 3].py
@@ -42,19 +42,32 @@ choice = st.selectbox("Select a column to plot", ["All"] + numeric_columns)
 
 # --- Step 4: Plot ---
 
-fig, ax = plt.subplots(figsize=(14, 7))
+# --- Step 4: Plot ---
+fig, ax = plt.subplots(figsize=(12, 5))
 
 if choice == "All":
     subset_plot[numeric_columns].plot(ax=ax)
-    ax.legend(bbox_to_anchor=(1, 1), loc='upper left')  # move outside
+    ax.legend(bbox_to_anchor=(1, 1), loc='upper left')
 else:
     subset_plot[choice].plot(ax=ax, label=choice)
-    ax.legend(bbox_to_anchor=(1, 1), loc='upper left')  # move outside
+    ax.legend(bbox_to_anchor=(1, 1), loc='upper left')
 
-ax.set_title("Graph of all the variables" if choice == "All" else f"Graph of {choice}")
-ax.set_xlabel("Time")
-ax.set_ylabel("Range of values of the variables")
-ax.grid(True, linestyle="--", alpha=0.5)
+# ----- Customize axis labels -----
+ax.set_title("Graph of all the variables" if choice == "All" else f"Graph of {choice}",
+             fontsize=14, pad=15)  # title font size
+
+ax.set_xlabel("Time", fontsize=12, labelpad=10)   # x-axis label font size + padding
+ax.set_ylabel("Range of values of the variables", fontsize=12, labelpad=10)  # y-axis
+
+# ----- Customize tick labels (numbers) -----
+ax.tick_params(axis="x", labelsize=10)  # font size of x-axis ticks
+ax.tick_params(axis="y", labelsize=10)  # font size of y-axis ticks
+
+# ----- Move x-axis position (example: put it on top) -----
+ax.xaxis.set_ticks_position("top")   # can be 'bottom' or 'top'
+ax.xaxis.set_label_position("top")   # move x-axis label too
+
+ax.grid(True, linestyle="--", alpha=0.6)
 
 st.pyplot(fig)
 
