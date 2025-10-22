24806e2e7af6bdb627cce3b856fc218e0fd4e726
Mobashra Abeer <moabe2274@nmbu.no>
2025-10-22 22:06:58 +0200

    check

--- pages/4_Meme_[page 4].py
+++ pages/4_Meme_[page 4].py
@@ -1,24 +1,4 @@
 import streamlit as st
-import pymongo
-import pandas as pd
 
-# Load MongoDB secrets
-mongo_uri = st.secrets["mongo"]["uri"]
-mongo_db = st.secrets["mongo"]["database"]
-mongo_collection = st.secrets["mongo"]["collection"]
-
-# Connect to MongoDB
-client = pymongo.MongoClient(mongo_uri)
-db = client[mongo_db]
-collection = db[mongo_collection]
-
-# Load data into a DataFrame
-data = pd.DataFrame(list(collection.find()))
-
-# Optional: Drop the MongoDB default `_id` column if not needed
-if "_id" in data.columns:
-    data = data.drop(columns=["_id"])
-
-# Preview
-st.write("Sample data from MongoDB:")
-st.dataframe(data.head())
+# Display all loaded secrets as a dictionary
+st.write("Loaded secrets:", st.secrets)
