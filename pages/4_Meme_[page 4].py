515983e954dc54877298a14686928516f07e8af3
Mobashra Abeer <moabe2274@nmbu.no>
2025-10-22 22:19:15 +0200

    implementation 1

--- pages/4_Meme_[page 4].py
+++ pages/4_Meme_[page 4].py
@@ -1,16 +1,3 @@
-from pyspark.sql import SparkSession
-
-# Same Spark session config as before
-spark = SparkSession.builder \
-    .appName("SparkCassandraWriteApp") \
-    .config("spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.12:3.5.1") \
-    .config("spark.cassandra.connection.host", "localhost") \
-    .config("spark.cassandra.connection.port", "9042") \
-    .config("spark.sql.extensions", "com.datastax.spark.connector.CassandraSparkExtensions") \
-    .config("spark.sql.catalog.mycatalog", "com.datastax.spark.connector.datasource.CassandraCatalog") \
-    .getOrCreate()
-
-print("successful")
 
 
 
